05d26d82fd64f08127540848d202ebc002c793bb
ji-hyup <newnyup@gmail.com>
2026-05-02 15:08:33 +0900

    01 실수로 복붙 잘못함

--- leetcode3/변지협/01_two_sum.py
+++ leetcode3/변지협/01_two_sum.py
@@ -1,33 +1,21 @@
 '''
 1. 아이디어 :
-- 정렬 후 투포인터
+- 완전 탐색
 
 2. 시간복잡도 :
     O(n^2)
 
 3. 자료구조/알고리즘 :
-- 투포인터
+
 '''
 
-class Solution:
-    def threeSum(self, nums: list[int]) -> list[list[int]]:
-        nums.sort()
-        # print(nums)
-        _set = set()
-        for i in range(len(nums) - 2):
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
-            left, right = i + 1, len(nums) - 1
 
-            while left < right:
-                _3sum = nums[i] + nums[left] + nums[right]
-                if _3sum < 0:
-                    left += 1
-                elif _3sum > 0:
-                    right -= 1
-                else:
-                    _set.add(tuple(sorted([nums[i],nums[left],nums[right]])))
-                    left += 1
-        
-        # print(_set)
-        return [list(s) for s in _set]
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums) - 1):
+            for j in range(len(nums) - i - 1):
+                sum = nums[i]
+                sum += nums[i+j+1]
+                if sum == target:
+                    return [i,i+j+1]
+            
