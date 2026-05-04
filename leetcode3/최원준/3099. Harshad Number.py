b65f8df01c9194f2b38aefb7e4ba97a014d52530
724thomas <724thomas@gmail.com>
2026-05-04 17:51:20 +0900

    [최원준]Day04

--- leetcode3/최원준/3099. Harshad Number.py
+++ leetcode3/최원준/3099. Harshad Number.py
@@ -17,3 +17,9 @@ class Solution:
     def sumOfTheDigitsOfHarshadNumber(self, x: int) -> int:
         total = sum([int(char) for char in str(x)])
         return total if x % total == 0 else -1
+
+class Solution:
+    def sumOfTheDigitsOfHarshadNumber(self, x: int) -> int:
+        n, total = x, 0
+        while n: total, n = total + n % 10, n // 10
+        return total if x % total == 0 else -1
