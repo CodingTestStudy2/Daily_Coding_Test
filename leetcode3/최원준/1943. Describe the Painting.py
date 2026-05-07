4528ed8a045a28968a30a298db00c7b0c441576f
724thomas <724thomas@gmail.com>
2026-05-07 22:24:38 +0900

    [최원준]Day07

---
+++ leetcode3/최원준/1943. Describe the Painting.py
@@ -0,0 +1,48 @@
+#
+
+'''
+1. 아이디어 :
+- prefix sum을 구합니다.
+- 같은 값이더라도 색상 set이 다르면 연속된 값이 아닌데, 조건중 color is distinct의 힌트를 보면,
+- segment의 시작과 끝을 만나게 되면 항상 끊긴다는 의미를 갖습니다.
+- 계산한 prefix sum을 순회하면서 구간들을 ans에 넣다가 segment의 시작/끝을 만나면 새로운 구간으로 만듭니다.
+
+2. 시간복잡도 :
+    O(nlogn)
+
+3. 자료구조/알고리즘 :
+- 누적합 / 해시셋
+
+'''
+
+
+class Solution:
+    def splitPainting(self, segments: List[List[int]]) -> List[List[int]]:
+        ans = []
+        end = max(segment[1] for segment in segments)
+
+        prefix_sum = [0] * (end+1)
+        stops = set()
+
+        for left, right, color in segments:
+            prefix_sum[left]+=color
+            prefix_sum[right]-=color
+            stops.add(left)
+            stops.add(right)
+
+        stops = [stop for stop in stops]
+        stops.sort()
+
+        for i in range(1, len(prefix_sum)):
+            prefix_sum[i] += prefix_sum[i-1]
+
+        prev = -1
+        for curr in stops:
+            if prev == -1:
+                prev = curr
+                continue
+            if prefix_sum[prev] != 0:
+                ans.append([prev, curr, prefix_sum[prev]])
+            prev = curr
+
+        return ans
