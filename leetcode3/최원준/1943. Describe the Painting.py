a7269d376c72559fd59d7f22bd6d707ff5f433f2
724thomas <724thomas@gmail.com>
2026-05-07 22:36:55 +0900

    [최원준]Day07

--- leetcode3/최원준/1943. Describe the Painting.py
+++ leetcode3/최원준/1943. Describe the Painting.py
@@ -8,7 +8,7 @@
 - 계산한 prefix sum을 순회하면서 구간들을 ans에 넣다가 segment의 시작/끝을 만나면 새로운 구간으로 만듭니다.
 
 2. 시간복잡도 :
-    O(nlogn)
+    O(n)
 
 3. 자료구조/알고리즘 :
 - 누적합 / 해시셋
@@ -21,28 +21,39 @@ class Solution:
         ans = []
         end = max(segment[1] for segment in segments)
 
-        prefix_sum = [0] * (end+1)
+        prefix_sum = [0] * (end + 1)
         stops = set()
 
         for left, right, color in segments:
-            prefix_sum[left]+=color
-            prefix_sum[right]-=color
+            prefix_sum[left] += color
+            prefix_sum[right] -= color
             stops.add(left)
             stops.add(right)
 
-        stops = [stop for stop in stops]
-        stops.sort()
+        # stops = [stop for stop in stops]
+        # stops.sort()
 
-        for i in range(1, len(prefix_sum)):
-            prefix_sum[i] += prefix_sum[i-1]
+        # for i in range(1, len(prefix_sum)):
+        #     prefix_sum[i] += prefix_sum[i - 1]
+
+        # prev = -1
+        # for curr in stops:
+        #     if prev == -1:
+        #         prev = curr
+        #         continue
+        #     if prefix_sum[prev] != 0:
+        #         ans.append([prev, curr, prefix_sum[prev]])
+        #     prev = curr
 
         prev = -1
-        for curr in stops:
-            if prev == -1:
-                prev = curr
-                continue
-            if prefix_sum[prev] != 0:
-                ans.append([prev, curr, prefix_sum[prev]])
-            prev = curr
+        for i in range(1, len(prefix_sum)):
+            prefix_sum[i] += prefix_sum[i - 1]
+            if i in stops:
+                if prev == -1:
+                    prev = i
+                    continue
+                if prefix_sum[prev] != 0:
+                    ans.append([prev, i, prefix_sum[prev]])
+                prev = i
 
         return ans
