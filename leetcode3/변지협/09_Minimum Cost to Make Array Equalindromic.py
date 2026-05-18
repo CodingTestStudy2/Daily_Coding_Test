7b4fe1c854059687cff419e6de06c7d645756ab3
ji-hyup <newnyup@gmail.com>
2026-05-18 13:05:04 +0900

    변지협2

--- leetcode3/변지협/09_Minimum Cost to Make Array Equalindromic.py
+++ leetcode3/변지협/09_Minimum Cost to Make Array Equalindromic.py
@@ -1,51 +1,56 @@
 '''
 1. 아이디어 :
-    평균값을 구해서 그걸 기준으로 팰린드롬을 만들어보고, 
-    그 팰린드롬과의 차이를 구해서 최소값을 구한다.
+    median 구해서 최소, 최대 palindrome 구해서 둘중에 더 작은거로 cost 계산
 2. 시간복잡도 :
-
+    n * log n - sort
 3. 자료구조/알고리즘 :
 '''
 class Solution:
     def minimumCost(self, nums: List[int]) -> int:
-        avg = sum(nums) / len(nums)
-        int_avg = int(avg)
-        str_avg = str(int_avg)
-
-        left = ''
-        
-        # len = 9 4개
-        for i in range(len(str_avg) // 2):
-            left += str_avg[i]
-        
-        print(left)
-
-        mid = str_avg[len(str_avg) // 2]
-        print("mid:",mid)
-        print("str_avg:",str_avg)
-        print("avg:", avg)
-
-        full_lst = []
-        if not left:
-            full_lst.append(mid)
-            full_lst.append(str(int(mid)+1))
-            full_lst.append(str(int(mid)-1))
+        sorted_nums = sorted(nums)
+        median = 0
+        if len(nums) %2 == 0:
+            median = sorted_nums[len(nums) //2 - 1] + sorted_nums[len(nums) //2]
+            median //= 2
         else:
-            for _str in [left, str(int(left) -1), str(int(left)+1)]:
-                if len(str_avg) % 2 != 0:
-                    full = _str + mid + _str[-1]
-                else:
-                    full = _str + _str[-1]
-                
-                full_lst.append(full)
+            median = sorted_nums[len(nums) //2]
 
-        # print(full_lst)
+        short = median
+        while True:
+            if self.is_pal(short):
+                break
+            
+            short -= 1
+        
+        long = median
+        while True:
+            if self.is_pal(long):
+                break
+            
+            long += 1
 
-        answers = []
-        for pan in full_lst:
-            ans =0
-            for i in nums:
-                ans += abs(int(pan) - i)
-            answers.append(ans)
+        # print(short, long)
+        
+        return min(sum([abs(num - short) for num in nums]), sum([abs(num - long) for num in nums]))
 
-        return min(answers)
+    def is_pal(self, num: int) -> bool:
+        if len(str(num)) == 1:
+            return True
+        elif len(str(num)) %2 == 0 :
+            # 123321 - 6 절반 = 3
+            left = str(num)[:len(str(num))//2]
+            right = str(num)[len(str(num))//2:]
+            if left == right[::-1]:
+                return True
+        else:
+            # 1234321 - 7 절반 = 3
+            left = str(num)[:len(str(num))//2]
+            right = str(num)[len(str(num))//2 + 1:]
+            if left == right[::-1]:
+                return True
+        
+        return False
+            
+            
+            
+            
