b52bfb8fac288d62873f581e2ee27d11fc653f96
SinnoLn <ok373737@naver.com>
2026-06-10 14:29:33 +0900

    [이진희] Day09

---
+++ leetcode3/이진희/3658_GCD of Odd and Even Sums.java
@@ -0,0 +1,15 @@
+/*
+
+1. 아이디어 : n마다 홀수와 짝수의 합을 생각해보면 그 차이는 n이라는 규칙이 나온다
+
+2. 시간복잡도 : O(1)
+
+3. 자료구조/알고리즘 : 규칙
+
+ */
+
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+}
