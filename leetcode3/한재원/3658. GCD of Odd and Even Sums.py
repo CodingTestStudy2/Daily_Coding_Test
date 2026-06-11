c632c7a4a8e562b2bfe3e3e0d16f825e3141f991
Jaewon Han <jaewonhan20@gmail.com>
2026-06-11 00:32:33 -0500

    [한재원] Day 09

---
+++ leetcode3/한재원/3658. GCD of Odd and Even Sums.py
@@ -0,0 +1,13 @@
+'''
+input: integer
+output: integer - GCD of sumOdd and sumEven
+
+4
+sumOdd: 1+3+5+7 = 16
+sumEven: 2+4+6+8 = 20
+
+'''
+class Solution:
+    def gcdOfOddEvenSums(self, n: int) -> int:
+        return n
+        
