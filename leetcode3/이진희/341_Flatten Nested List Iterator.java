94c13667c7a2a7ad08b5106acd930c0685e5b9a1
SinnoLn <ok373737@naver.com>
2026-06-16 22:30:59 +0900

    주석추가

--- leetcode3/이진희/341_Flatten Nested List Iterator.java
+++ leetcode3/이진희/341_Flatten Nested List Iterator.java
@@ -1,3 +1,20 @@
+/*
+
+1. 아이디어 : 주어진 인터페이스 활용, 아래 메소드를 완성
+              1. public NestedIterator(List<NestedInteger> nestedList) -> nestedList 초기화
+              2. public Integer next() -> 다음 수 출력
+              3. public boolean hasNext() -> 현재 위치에서 nested list에 다음 Integer가 존재시 return true
+
+              이때, 리스트 원소를 뒤부터 넣어줘야하는데, 원래 순서로 풀으려면, 나중에 들어간 요소가 먼저나와야 하기 때문
+
+2. 시간복잡도 : 1. NestedIterator -> O(N)
+                2. Integer next() -> O(1)
+                3. boolean hasNext() -> O(N)
+
+3. 자료구조/알고리즘 : Deque(Stack 처럼 활용)
+
+ */
+
 public class NestedIterator implements Iterator<Integer> {
     private Deque<NestedInteger> deque;
 
