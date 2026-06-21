6cd2d5225ff93a8802aa0cac6c1f99d37047659c
Won Joon Thomas Choi <113500771+724thomas@users.noreply.github.com>
2026-06-22 01:28:23 +0900

    Add function to count odd value cells in matrix
    Implement a function to count cells with odd values in a matrix after applying given indices.

---
+++ leetcode3/최원준/1252. Cells with Odd Values in a Matrix.py
@@ -0,0 +1,32 @@
+#
+
+'''
+1. 아이디어 :
+
+
+2. 시간복잡도 :
+    O()
+
+3. 자료구조/알고리즘 :
+
+
+'''
+
+class Solution:
+    def oddCells(self, m: int, n: int, indices: List[List[int]]) -> int:
+        m_count = [0] * m
+        n_count = [0] * n
+
+        for a, b in indices:
+            m_count[a] += 1
+            n_count[b] += 1
+
+        count = 0
+
+        for row in range(m):
+            for col in range(n):
+                value = m_count[row] + n_count[col]
+                if value % 2 == 1:
+                    count += 1
+
+        return count
