389b67f3e7c737bbb82ffbd18c5d5b40263db939
unknown <1326cy@gmail.com>
2026-08-03 23:35:06 +0900

    day05

---
+++ leetcode3/남효정/3875. Construct Uniform Parity Array I.py
@@ -0,0 +1,9 @@
+'''
+1. 아이디어 : (짝, 짝), (홀, 홀) => 문제 없음. (홀, 짝) => 언제나 (홀, 홀) 됨
+2. 시간복잡도 :
+3. 자료구조/알고리즘 :
+'''
+
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        return True
