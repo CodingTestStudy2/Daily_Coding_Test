1f66252fd22a070368895e369b7b7ebb112031e9
rimogsu <newnyup@gmail.com>
2026-08-06 22:22:41 +0900

    주석 추가

--- leetcode3/변지협/v2/4006. Count Valid Prefixes.py
+++ leetcode3/변지협/v2/4006. Count Valid Prefixes.py
@@ -1,3 +1,15 @@
+
+'''
+1. 아이디어 :
+0과 1의 개수가 1 이하로 차이나면 ans에 넣는 식으로 구한다.
+
+2. 시간복잡도 :
+    o(n^2)
+
+3. 자료구조/알고리즘 :
+완전탐색
+'''
+
 class Solution(object):
     def countValidPrefixes(self, s):
         """
--- leetcode3/변지협/v2/648. Replace Words.py
+++ leetcode3/변지협/v2/648. Replace Words.py
@@ -1,3 +1,16 @@
+'''
+1. 아이디어 :
+prefix에 있는 단어를 완전 탐색으로 찾아서 바꾸는 형태로 진행한다.
+for i in lst 이렇게 하면 시간 초과 나는데
+for i in dic 이렇게 하면 시간 초과가 안난다.
+왜냐하면 dic은 해시맵이기 때문에 검색 시간이 o(1)이 걸리고, lst는 리스트기 때문에 검색 시간이 o(n)이 걸린다.
+
+2. 시간복잡도 :
+    o(n^2)
+
+3. 자료구조/알고리즘 :
+완전탐색
+'''
 class Solution(object):
     def replaceWords(self, dictionary, sentence):
         """
