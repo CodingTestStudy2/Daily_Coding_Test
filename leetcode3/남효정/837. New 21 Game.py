5bdf39485a3c2d2fa3775bd40bbfb71cd052bc84
unknown <1326cy@gmail.com>
2026-08-11 22:44:05 +0900

    오타 수정

--- leetcode3/남효정/837. New 21 Game.py
+++ leetcode3/남효정/837. New 21 Game.py
@@ -1,7 +1,7 @@
 # 풀이 실패
 class Solution:
     def new21Game(self, n: int, k: int, maxPts: int) -> float:
-        if k == 0 or n >= k + maxPtsL
+        if k == 0 or n >= k + maxPts:
             return 1.0
 
         dp = [0.0] * (n + 1)
