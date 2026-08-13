0aa94809a5a292b89336b1a83348f447cd65b1f8
choiminji <chlals9930@gmail.com>
2026-08-13 22:36:21 +0900

    Day15

---
+++ leetcode3/최민지/1539.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        count = 0
+        for i in range(1,1001):
+            if i not in arr:
+                count += 1
+                if count == k:
+                    return i
+        
