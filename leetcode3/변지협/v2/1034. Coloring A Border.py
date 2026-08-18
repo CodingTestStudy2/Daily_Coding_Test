b1fd13c4ecf6ec9d2661d88b9c97c93d08205348
rimogsu <newnyup@gmail.com>
2026-08-18 23:37:51 +0900

    1034 재풀이

--- leetcode3/변지협/v2/1034. Coloring A Border.py
+++ leetcode3/변지협/v2/1034. Coloring A Border.py
@@ -6,26 +6,34 @@ class Solution:
         
         m, n = len(grid), len(grid[0])
         
-        visited = [[0] * n] * m
-        print(visited)
+        visited = [[0] * n for _ in range(m)]
+        tmp = [[0] * n for _ in range(m)]
         
         queue.append((col,row))
+        c = grid[row][col]
         dirs = [(0,1),(0,-1),(1,0),(-1,0)]
+        
         
         while True:
             if len(queue) == 0:
                 break
             
-            x,y = queue.pop()
+            x,y = queue.popleft()
             visited[y][x] = 1
             
-            if x == 0 or x == n or y == 0 or y == m :
-                grid[y][x] = color
+            if x == 0 or x == n-1 or y == 0 or y == m-1:
+                tmp[y][x] = 1
+            elif not (grid[y+1][x] == c and grid[y-1][x] == c and grid[y][x+1] == c and grid[y][x-1] == c):
+                tmp[y][x] = 1
 
             for dx,dy in dirs:
-                if  
-            
-            
-            
+                if 0 <= x + dx < n and 0 <= y + dy < m and visited[y+dy][x+dx] == 0 and grid[y+dy][x+dx] == c:
+                    queue.append((x+dx,y+dy))
+
+        for y in range(m):
+            for x in range(n):
+                if tmp[y][x] == 1:
+                    grid[y][x] = color
+
         return grid
         
