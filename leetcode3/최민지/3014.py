77631c550aa095fadbb63a28b4385af339341b6e
choiminji <chlals9930@gmail.com>
2026-08-24 23:52:27 +0900

    Day26

---
+++ leetcode3/최민지/3014.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        n = len(word)
+
+        result = 0
+
+        for i in range(n):
+            result += i // 8 + 1
+
+        return result
+        
