293496281345aa8bb53e7947cb2551aa8570de36
masterMJ <okwow123@naver.com>
2026-08-28 09:17:02 +0900

    Update 3996. Even Number of Knight Moves.py

--- leetcode3/김민제/3996. Even Number of Knight Moves.py
+++ leetcode3/김민제/3996. Even Number of Knight Moves.py
@@ -2,7 +2,7 @@ class Solution:
     def canReach(self, start: list[int], target: list[int]) -> bool:
         start_parity = (start[0]+start[1])%2
         target_paritiy = (target[0]+target[1])%2
-
+        #test
         if start_parity == target_paritiy:
             return True
         else:
