b7ab04fb96b22bbdd073b47ad23ad68e2b4f204a
choiminji <chlals9930@gmail.com>
2026-09-18 23:50:20 +0900

    0918

---
+++ leetcode3/최민지/226.py
@@ -0,0 +1,11 @@
+class Solution:
+    def invertTree(self, root: TreeNode | None) -> TreeNode | None:
+        if root is None:
+            return None
+
+        root.left, root.right = root.right, root.left
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
